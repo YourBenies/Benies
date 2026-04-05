8720f1054e4371f0cfd60d24318d987e23ba396a
YourBenies <broemen@yourbenies.com>
2026-04-05 09:59:45 -0500

    Make onOpenWaitlist prop optional in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import BeniesLogo from './BeniesLogo'
 
 interface Props {
-  onOpenWaitlist: () => void
+  onOpenWaitlist?: () => void
 }
 
 const navLinks = [
