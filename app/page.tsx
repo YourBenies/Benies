e8cbadb5a458865e88b324a930e3977a8caa7ec2
YourBenies <broemen@yourbenies.com>
2026-04-05 08:53:57 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
-import Navbar from '@/components/Navbar'
-import HeroSection from '@/components/HeroSection'
-import CredibilityStrip from '@/components/CredibilityStrip'
-import ProblemSection from '@/components/ProblemSection'
-import HowItWorks from '@/components/HowItWorks'
-import FeaturesSection from '@/components/FeaturesSection'
-import OutcomesSection from '@/components/OutcomesSection'
-import PrivacySection from '@/components/PrivacySection'
-import FAQSection from '@/components/FAQSection'
-import CTASection from '@/components/CTASection'
-import Footer from '@/components/Footer'
+'use client'
 
-export default function Home() {
+import { useState } from 'react'
+import Navbar from "@/components/Navbar"
+import HeroSection from "@/components/HeroSection"
+import CredibilityStrip from "@/components/CredibilityStrip"
+import ProblemSection from "@/components/ProblemSection"
+import HowItWorks from "@/components/HowItWorks"
+import FeaturesSection from "@/components/FeaturesSection"
+import OutcomesSection from "@/components/OutcomesSection"
+import PrivacySection from "@/components/PrivacySection"
+import FAQSection from "@/components/FAQSection"
+import CTASection from "@/components/CTASection"
+import Footer from "@/components/Footer"
+import WaitlistModal from "@/components/WaitlistModal"
+
+export default function Index() {
+  const [modalOpen, setModalOpen] = useState(false)
+
   return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
+    <div className="min-h-screen bg-background">
+      <WaitlistModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
+      <Navbar onOpenWaitlist={() => setModalOpen(true)} />
       <main>
         <HeroSection />
         <CredibilityStrip />
@@ -23,7 +30,7 @@ export default function Home() {
         <OutcomesSection />
         <PrivacySection />
         <FAQSection />
-        <CTASection />
+        <CTASection onOpenWaitlist={() => setModalOpen(true)} />
       </main>
       <Footer />
     </div>
